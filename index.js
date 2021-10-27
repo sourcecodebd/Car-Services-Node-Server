73419117f30ba55da601d9098eb4d4ad76554e4f
sourcebd <nafimahmud1996@gmail.com>
2021-10-27 07:06:40 +0600

    node-server-done

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Running Car Services BD server...');
+})
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ombkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
+const Run_Car_Services_Own_Database = async () => {
     try {
         await client.connect();
-        const database = client.db('carMechanic');
-        const servicesCollection = database.collection('services');
+        const database = client.db('CarMechanic');
+        const servicesCollection = database.collection('Services');
 
-        // GET API
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
-        });
-
-        // GET Single Service
-        app.get('/services/:id', async (req, res) => {
+        })
+        app.get('/services/details/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('getting specific service', id);
             const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
-            res.json(service);
+            res.send(service);
         })
-
-        // POST API
         app.post('/services', async (req, res) => {
-            const service = req.body;
-            console.log('hit the post api', service);
-
-            const result = await servicesCollection.insertOne(service);
-            console.log(result);
-            res.json(result)
-        });
-
-        // DELETE API
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.json(result);
+        })
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+        app.put('/services/update/:id', async (req, res) => {
+            const updatedServices = req.body;
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedServices.name,
+                    price: updatedServices.price,
+                    description: updatedServices.description,
+                    time: updatedServices.time,
+                    img_url: updatedServices.img_url
+                }
+            };
+            const result = await servicesCollection.updateOne(filter, updateDoc, option);
             res.json(result);
         })
 
@@ -61,37 +67,8 @@ async function run() {
         // await client.close();
     }
 }
-
-run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send('Running Genius Server');
-});
-
-app.get('/hello', (req, res) => {
-    res.send('hello updated here')
-})
+Run_Car_Services_Own_Database().catch(console.dir);
 
 app.listen(port, () => {
-    console.log('Running Genius Server on port', port);
+    console.log('Listening Car Services BD server on port ' + port);
 })
-
-/*
-one time:
-1. heroku account open
-2. Heroku software install
-Every project
-1. git init
-2. .gitignore (node_module, .env)
-3. push everything to git
-4. make sure you have this script:  "start": "node index.js",
-5. make sure: put process.env.PORT in front of your port number
-6. heroku login
-7. heroku create (only one time for a project)
-8. command: git push heroku main
-----
-update:
-1. save everything check locally
-2. git add, git commit-m", git push
-2. git push heroku main
-*/
---
+++ index2.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+        // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Genius Server');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('hello updated here')
+})
+
+app.listen(port, () => {
+    console.log('Running Genius Server on port', port);
+})
+
+/*
+one time:
+1. heroku account open
+2. Heroku software install
+Every project
+1. git init
+2. .gitignore (node_module, .env)
+3. push everything to git
+4. make sure you have this script:  "start": "node index.js",
+5. make sure: put process.env.PORT in front of your port number
+6. heroku login
+7. heroku create (only one time for a project)
+8. command: git push heroku main
+----
+update:
+1. save everything check locally
+2. git add, git commit-m", git push
+2. git push heroku main
+*/
