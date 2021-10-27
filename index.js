4721847962517ab0a0462b712648223ff26c32a2
sourcebd <nafimahmud1996@gmail.com>
2021-10-27 07:42:31 +0600

    node-server-done

--- index.js
+++ index.js
@@ -72,3 +72,24 @@ Run_Car_Services_Own_Database().catch(console.dir);
 app.listen(port, () => {
     console.log('Listening Car Services BD server on port ' + port);
 })
+
+
+/*
+one time:
+1. heroku account open
+2. Heroku software install
+Every project
+1. git init
+2. .gitignore (node_module, .env)
+3. push everything to git
+4. make sure you have this script:  "start": "node index.js",
+5. make sure: put process.env.PORT in front of your port number
+6. heroku login
+7. heroku create (only one time for a project)
+8. command: git push heroku main
+----
+update:
+1. save everything check locally
+2. git add, git commit-m", git push
+2. git push heroku main
+*/
